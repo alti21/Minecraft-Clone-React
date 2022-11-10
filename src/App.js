45a8a55fe6d6e925de8276473194684502414b05
Altemush Bhatti <altemush@gmail.com>
2022-11-09 19:20:20 -0800

    feat: add a Sky to the background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
     <>
-      <div>Outside Canvas</div>
+      {/* We cannot put an HTML tag inside the Canvas, else we get an error */}
       <Canvas>
+        <Sky sunPosition={[100, 100, 20]}/>
       </Canvas>
     </>
   );
